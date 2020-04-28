6b328f197b67f2a8d8e4f63f6ff22926a454b8ee
Robert Roose <robert@shivercube.com>
2020-04-28 16:05:59 -0300

    Wait for Vault before pulling from it

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# Wait for Vault to be ready so we can pull config from it
+wait-for-it "vault.in.okinta.ge:7020"
+
 # Grab LogDNA's ingestion key so we can forward logs
 LOGDNA_INGESTION_KEY=$(wget -q -O - http://vault.in.okinta.ge:7020/api/kv/logdna_ingestion_key)
 export LOGDNA_INGESTION_KEY
